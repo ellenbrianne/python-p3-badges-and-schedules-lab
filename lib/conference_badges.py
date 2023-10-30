3b75a621158ca0fad2d8ac00fdda13f24ffecf4a
Ellen Bennett <bennette831@gmail.com>
2023-10-30 03:10:26 -0600

    use lc, for and while loops to iterate through lists

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,20 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    i = 1
+    new_l = []
+    while i < 9:
+        for name in names:
+            new_l.append(f"Hello, {name}! You'll be assigned to room {i}!")
+            i += 1
+    return new_l
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for s in badges: print(s)
+    for s in rooms: print(s)
